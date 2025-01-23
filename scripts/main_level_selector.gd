b58e1745b2151ea846244ca42f19b763099c8468
James Perez <danjame@hotmail.com>
2025-01-22 22:57:30 -0500

    added scripts

---
+++ scripts/main_level_selector.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Hacer que la cámara coincida con la posición del personaje
+	var player_position = $GotasTheCat3D.position
+	#print("player: ",  player_position)
+	
+	var camera_position = $GotasTheCat3D/CameraController.position
+	print("camera: ", camera_position)
+	
+	camera_position.x = lerp_angle(camera_position.x, atan2(player_position.y, player_position.z), 0.15)
+	camera_position.y = lerp_angle(camera_position.y, atan2(player_position.z, player_position.x), 0.1)
+
+	$GotasTheCat3D/CameraController.position = camera_position
